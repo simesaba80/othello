b61eafbc529c609bd93cd78b7cbe6eca2690ddf5
simesaba80 <simesaba1120@gmail.com>
2023-06-22 16:46:16 +0900

    add関数用のヘッダファイルを追加しました

---
+++ othello.h
@@ -0,0 +1,13 @@
+#ifndef _OTHELLO_
+#define _OTHELLO_
+
+void left(board *, int, int, int);
+void leftup(board *, int, int, int);
+void up(board *, int, int, int);
+void rightup(board *, int, int, int);
+void right(board *, int, int, int);
+void rightdown(board *, int, int, int);
+void down(board *, int, int, int);
+void leftdown(board *, int, int, int);
+
+#endif
